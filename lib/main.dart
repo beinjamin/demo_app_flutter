863496f3137feeddfbdd046bcd2f1358fa3e66d5
beinjamin <jauresbeinjamin@gmail.com>
2022-01-09 19:38:25 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
   runApp(MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
