edd013e8d404e07f1f36b39af53ba3489f48b8dd
beinjamin <jauresbeinjamin@gmail.com>
2022-01-23 15:24:28 +0100

    Ajout un wideget qui consomme les notification

--- lib/favoriteWidget.dart
+++ lib/favoriteWidget.dart
@@ -1,82 +1,48 @@
+import 'package:demo_app_flutter/favoriteChangeNotifier.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FavoriteIconWidget extends StatefulWidget {
   _FavoriteIconWidgetState createState() => _FavoriteIconWidgetState();
 }
 
 class _FavoriteIconWidgetState extends State<FavoriteIconWidget> {
-  bool _isFavorited;
+  late bool _isFavorited;
 
-  void _toggleFavorite() {
+  void _toggleFavorite(FavoriteChangeNotifier _notifier) {
     setState(() {
       if (_isFavorited) {
         _isFavorited = false;
       } else {
         _isFavorited = true;
       }
+
+      _notifier.isFavorite = _isFavorited;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(
-          icon:
-              _isFavorited ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
-          color: Colors.red,
-          onPressed: _toggleFavorite,
-        ),
-      ],
+    FavoriteChangeNotifier _notifier =
+        Provider.of<FavoriteChangeNotifier>(context);
+    _isFavorited = _notifier.isFavorited;
+    return IconButton(
+      icon: _isFavorited ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
+      color: Colors.red,
+      onPressed: () => _toggleFavorite(_notifier),
     );
   }
 }
 
 class FavoriteTextWidget extends StatefulWidget {
-  final bool isFavorited;
-  final int favoriteCount;
-
-  const FavoriteTextWidget({
-    Key? key,
-    required this.isFavorited,
-    required this.favoriteCount,
-  }) : super(key: key);
-
-  _FavoriteTextWidgetState createState() => _FavoriteTextWidgetState(
-        this.isFavorited,
-        this.favoriteCount,
-      );
+  _FavoriteTextWidgetState createState() => _FavoriteTextWidgetState();
 }
 
 class _FavoriteTextWidgetState extends State<FavoriteTextWidget> {
-  bool _isFavorited;
-  int _favoriteCount;
-  _FavoriteTextWidgetState(this._isFavorited, this._favoriteCount);
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _isFavorited = false;
-        _favoriteCount -= 1;
-      } else {
-        _isFavorited = true;
-        _favoriteCount += 1;
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(
-          icon:
-              _isFavorited ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
-          color: Colors.red,
-          onPressed: _toggleFavorite,
-        ),
-        Text('$_favoriteCount')
-      ],
-    );
+    return Consumer<FavoriteChangeNotifier>(
+        builder: (context, notifier, _) =>
+            Text(notifier.favoriteCount.toString()));
   }
 }
